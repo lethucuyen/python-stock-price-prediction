d3b374d7681c2d8045c119933c5ef8a8b2806a0a
tutruongmugi <khongthenho304@gmail.com>
2021-07-21 20:59:49 +0700

    RNN & BRNN model

---
+++ build_RNN_model.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import datetime as dt
+import pandas_datareader as web
+
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM,Bidirectional
+from keras import Input
+
+from keras import Input
+
+#load Data
+
+stocks = ['NOK', 'TSLA','PEP','AMZN','GPS','HSBC']
+
+start = dt.datetime(2012,1,1)
+end = dt.datetime(2020,1,1)
+
+data = web.DataReader(stocks,'yahoo',start,end)
+
+print("Data: ",data['Close'])
+
+#Prepare Data
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(data['Close']['NOK'].values.reshape(-1,1))
+
+prediction_days=60
+
+x_train=[]
+y_train=[]
+
+for x in range(prediction_days,len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x,0])
+    y_train.append(scaled_data[x,0])
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
+
+#Build the model BLSTM
+
+model=Sequential()
+model.add(Bidirectional(LSTM(units=50, return_sequences=True), batch_input_shape=(None, 50, 5)))
+model.add(Dropout(0.3))
+model.add(Bidirectional(LSTM(units=50, return_sequences=False)))
+model.add(Dropout(0.3))
+model.add(Bidirectional(LSTM(units=50, return_sequences=True)))
+model.add(Dropout(0.3))
+model.add(Dense(units=1)) # prediction of the next closing price
+
+model.compile(optimizer='adam',loss='mean_squared_error')
+model.fit(x_train,y_train,epochs=25,batch_size=32)
+
+model.save("saved_lstm_closed_model_NOK.h5")
+
+
+
