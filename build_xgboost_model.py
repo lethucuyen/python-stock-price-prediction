c54add19777694cee59590994bf3b945e25efe98
tutruongmugi <khongthenho304@gmail.com>
2021-08-17 00:36:23 +0700

    update xgboost model

--- build_xgboost_model.py
+++ build_xgboost_model.py
@@ -9,48 +9,48 @@ import pickle
 start = dt.datetime(2012,1,1)
 end = dt.datetime(2020,1,1)
 
-df = web.DataReader("NFLX",'yahoo',start,end)
+df = web.DataReader("NOK",'yahoo',start,end)
 
+df = df[['Adj Close']].copy()
+df['target']=df['Adj Close'].shift(-1)
+df.dropna(inplace=True)
+train=df.values
 print("df ", df)
-dataset=df.values
-df['Adj Close']=df['Adj Close'].shift(-1)
-print("shift close", df)
-df = df[:-1]
+print(len(train))
 
-print(":-1 close", df)
-
-drop_cols = ['Volume','Close']
-df = df.drop(drop_cols, 1)
-# split data into X and y
-datasetY = df['Adj Close'].copy()
-datasetX = df.drop(['Adj Close'], 1)
-
-Y = datasetY.values
-X = datasetX.values
-
-model = XGBRegressor()
+X = train[:,:-1]
+Y = train[:,-1]
+print("X: ",X)
+model = XGBRegressor(objective="reg:squarederror",n_estimators=1000)
 model.fit(X, Y)
-pickle.dump(model, open("XGB_NFLX_Model.pkl", "wb"))
+pickle.dump(model, open("XGB_NOK_Model.pkl", "wb"))
 
 y_pred = model.predict(X)
 print(Y)
 print(y_pred)
 
-# split data into train and test sets
-# seed = 7
-# test_size = 0.33
-# X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
 
-# eval_set = [(X_train, y_train)]
-# fit model no training data
-
-
-
+# dataset=df.values
+# df['Adj Close']=df['Adj Close'].shift(-1)
+# print("shift close", df)
+# df = df[:-1]
+#
+# print(":-1 close", df)
+#
+# drop_cols = ['Volume','Close']
+# df = df.drop(drop_cols, 1)
+# # split data into X and y
+# datasetY = df['Adj Close'].copy()
+# datasetX = df.drop(['Adj Close'], 1)
+#
+# Y = datasetY.values
+# X = datasetX.values
+#
 # model = XGBRegressor()
 # model.fit(X, Y)
-# model.save_model("xgboost_closed_model.model")
-# # make predictions for test data
+# pickle.dump(model, open("XGB_NFLX_Model.pkl", "wb"))
+#
 # y_pred = model.predict(X)
 # print(Y)
 # print(y_pred)
-# print(model)
+
